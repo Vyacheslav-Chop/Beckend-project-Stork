79462bae706ebd09552dcf1f182b4f179e0d6ece
Dmitry Zinchenko <zinchenko89d@gmail.com>
2025-09-16 13:57:18 +0300

    authenticate

---
+++ src/middlewares/authenticate.js
@@ -0,0 +1,48 @@
+import createHttpError from 'http-errors';
+import { session } from '../db/models/session.js';
+import { user } from '../db/models/user.js';
+
+export const authenticate = async (req, res, next) => {
+    const authHeader = req.get('Authorization');
+
+    if (!authHeader) {
+        next(createHttpError(401, 'Please provide Authorization header'));
+        return;
+    }
+
+    const bearer = authHeader.split(' ')[0];
+    const token = authHeader.split(' ')[1];
+
+    if (bearer !== 'Bearer' || !token) {
+        next(createHttpError(401, 'Auth header should be of type Bearer'));
+        return;
+    }
+
+    const sessionAuth = await session.findOne({
+        accessToken: token
+    });
+
+    if (!sessionAuth) {
+        next(createHttpError(401, 'Session not found'));
+        return;
+    }
+
+    const isAccessTokenExpired =
+        new Date() > new Date(sessionAuth.accessTokenValidUntil);
+
+    if (isAccessTokenExpired) {
+        next(createHttpError(401, 'Access token expired'));
+        return;
+    }
+
+    const userAuth = await user.findById(sessionAuth.userId);
+
+    if (!userAuth) {
+        next(createHttpError(401));
+        return;
+    }
+
+    req.user = userAuth;
+
+    next();
+}
--- src/routers/index.js
+++ src/routers/index.js
@@ -4,17 +4,18 @@ import taskRouter from "./tasks.js";
 import userRouter from "./users.js";
 import diaryRouter from "./diaries.js";
 import weekRouter from "./week.js";
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
 
 router.use('/auth', authRouter);
 
-router.use('/tasks', taskRouter);
+router.use('/tasks', authenticate, taskRouter);
 
-router.use('/users', userRouter);
+router.use('/users', authenticate, userRouter);
 
-router.use('/diaries', diaryRouter);
+router.use('/diaries', authenticate, diaryRouter);
 
-router.use('/weeks', weekRouter);
+router.use('/weeks', authenticate, weekRouter);
 
 export default router;
