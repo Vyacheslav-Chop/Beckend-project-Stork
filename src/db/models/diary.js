128109a9ce94078d6137396c338f66328748818a
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-09-17 00:00:07 +0300

    feat(diary): update diary by ID with ID validation middleware

---
+++ src/db/models/diary.js
@@ -0,0 +1,29 @@
+import { model, Schema } from 'mongoose';
+
+const diarySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'emotions',
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'users',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const DiaryModel = model('diaries', diarySchema);
--- src/validation/helpers.js
+++ src/validation/helpers.js
@@ -1,6 +1,20 @@
 // Сюди пишемо допоміжні функції для валідації як було на уроках
 import Joi from 'joi';
+import { isValidObjectId } from 'mongoose';
 
 export const nameValidation = () => Joi.string().min(2).max(30);
 export const emailValidation = () => Joi.string().email();
 export const passwordValidation = () => Joi.string().min(6).max(30);
+
+export const objectIdValidation = () =>
+  Joi.string().custom((value, helpers) => {
+    const isValidId = isValidObjectId(value);
+
+    if (!isValidId) return helpers.message('Not valid objectId');
+
+    return value;
+  });
+
+export const titleDieryValidation = () => Joi.string().min(1).max(64);
+
+export const descriptionDiaryValidation = () => Joi.string().min(1).max(1000);
