b1f86a6eb9749dc8ef806588d04c124518d8c710
Viktor Momotenko <viktor.momotenko@gmail.com>
2025-09-17 02:56:37 +0100

    feat(swagger): set up swagger ui middleware for interactive api docs

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,19 +1,24 @@
 import fs from 'node:fs';
 import { SWAGGER_PATH } from '../constants/path.js';
 import swaggerUi from 'swagger-ui-express';
+import yaml from 'yaml';
 
 export const swaggerDocs = () => {
   try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    // const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
+    const fileContent = fs.readFileSync(SWAGGER_PATH, 'utf8');
+    const swaggerDoc = yaml.parse(fileContent);
 
     return [...swaggerUi.serve, swaggerUi.setup(swaggerDoc)];
   } catch (error) {
+    console.error('Failed to load Swagger Docs:', error);
     return (req, res) =>
       res.status(500).json({
         status: 500,
         message: 'Internal Sever Error!',
         data: {
-          message: error.message,
+          // message: error.message,
+          message: 'Failed to load Swagger documentation.',
         },
       });
   }
