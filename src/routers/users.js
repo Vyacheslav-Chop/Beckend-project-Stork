e5967fc00b32cc8d85c5333d4b75fede4d3572f7
Svitlana <missis.chuvaeva@ukr.net>
2025-09-17 10:26:50 +0300

    change 401.yaml

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { authenticate } from '../middlewares/authenticate.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { User } from '../db/models/user.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const userRouter = Router();
 
 userRouter.use('/', authenticate);
-
+userRouter.get('/user', validateBody(User), ctrlWrapper(getUserController));
 export default userRouter;
