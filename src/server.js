1523d38f10a752d827df657f9107c0298dd1b0c3
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-09-27 01:06:41 +0300

    update app.use for cors

--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-  app.use(cors());
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    }),
+  );
   app.use(cookieParser());
 
   app.use(
